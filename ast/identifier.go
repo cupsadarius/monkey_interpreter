51dc6fdb0e26f2824592c52eaf78f427eddd7ca1
Cupsa Darius <cupsadarius@gmail.com>
2022-08-15 19:35:19 +0300

    add let and return statement parsing

---
+++ ast/identifier.go
@@ -0,0 +1,14 @@
+package ast
+
+import "github.com/cupsadarius/monkey_interpreter/token"
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
