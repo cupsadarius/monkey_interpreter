458641797043797d29b8c7c125ec686854f8eb82
Cupsa Darius <cupsadarius@gmail.com>
2022-08-23 21:00:50 +0300

    parse infix operations

--- parser/expression.go
+++ parser/expression.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/cupsadarius/monkey_interpreter/ast"
+	"github.com/cupsadarius/monkey_interpreter/token"
 )
 
 type (
@@ -23,15 +24,56 @@ const (
 	CALL        // myFunction(x)
 )
 
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.LT_EQ:    LESSGREATER,
+	token.GT_EQ:    LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
+
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+
+	return LOWEST
+}
+
+func (p *Parser) curPrecedence() int {
+	if p, ok := precedences[p.curToken.Type]; ok {
+		return p
+	}
+
+	return LOWEST
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 
 	if prefix == nil {
-    p.noPrefixParseFnError(p.curToken.Type)
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 
 	leftExp := prefix()
+
+  for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+    infix := p.infixParseFns[p.peekToken.Type]
+
+    if infix == nil {
+    return nil
+    }
+
+    p.nextToken()
+
+    leftExp = infix(leftExp)
+  }
 
 	return leftExp
 }
@@ -41,43 +83,58 @@ func (p *Parser) parseIdentifier() ast.Expression {
 }
 
 func (p *Parser) parseIntegerLiteral() ast.Expression {
-  lit := &ast.IntegerLiteral{Token: p.curToken}
+	lit := &ast.IntegerLiteral{Token: p.curToken}
 
-  value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
 
-  if err != nil {
-    msg := fmt.Sprintf("could not parse %q as integer at %d: %d", p.curToken.Literal, p.curToken.Line, p.curToken.Column)
-    p.errors = append(p.errors, msg)
-    return nil
-  }
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer at %d: %d", p.curToken.Literal, p.curToken.Line, p.curToken.Column)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
 
-  lit.Value = value 
+	lit.Value = value
 
-  return lit
+	return lit
 }
 
 func (p *Parser) parseFloatLiteral() ast.Expression {
-  lit := &ast.FloatLiteral{Token: p.curToken}
+	lit := &ast.FloatLiteral{Token: p.curToken}
 
-  value, err := strconv.ParseFloat(p.curToken.Literal,  64)
+	value, err := strconv.ParseFloat(p.curToken.Literal, 64)
 
-  if err != nil {
-    msg := fmt.Sprintf("could not parse %q as float at %d: %d", p.curToken.Literal, p.curToken.Line, p.curToken.Column)
-    p.errors = append(p.errors, msg)
-    return nil
-  }
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as float at %d: %d", p.curToken.Literal, p.curToken.Line, p.curToken.Column)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
 
-  lit.Value = value 
+	lit.Value = value
 
-  return lit
+	return lit
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
-  expression := &ast.PrefixExpression{Token: p.curToken, Operator: p.curToken.Literal}
+	expression := &ast.PrefixExpression{Token: p.curToken, Operator: p.curToken.Literal}
 
+	p.nextToken()
+
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
+}
+
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+
+	expression := &ast.InfixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+		Left:     left,
+	}
+
+  precedence := p.curPrecedence()
   p.nextToken()
+  expression.Right = p.parseExpression(precedence)
 
-  expression.Right = p.parseExpression(PREFIX)
-
-  return expression
+	return expression
 }
