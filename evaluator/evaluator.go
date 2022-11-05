0b7adfb77abc1c623bfbe19a4af3b470c12ef9c2
Cupsa Darius <cupsadarius@gmail.com>
2022-11-05 17:48:49 +0200

    evaluate conditonal statements

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,6 +26,10 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		left := Eval(node.Left)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evaluateStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 
 		// Expressions
 	case *ast.IntegerLiteral:
@@ -167,4 +171,29 @@ func evaluateStatements(statements []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	}
+
+	return NULL
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -16,7 +16,7 @@ func testEval(input string) object.Object {
 	return Eval(program)
 }
 
-func testIntegerExpression(t *testing.T, obj object.Object, expected int64) bool {
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	result, ok := obj.(*object.Integer)
 	if !ok {
 		t.Errorf("object is not Integer, got=%T (%+v)", obj, obj)
@@ -31,7 +31,7 @@ func testIntegerExpression(t *testing.T, obj object.Object, expected int64) bool
 	return true
 }
 
-func testFloatExpression(t *testing.T, obj object.Object, expected float64) bool {
+func testFloatObject(t *testing.T, obj object.Object, expected float64) bool {
 	result, ok := obj.(*object.Float)
 	if !ok {
 		t.Errorf("object is not Float, got=%T (%+v)", obj, obj)
@@ -46,7 +46,7 @@ func testFloatExpression(t *testing.T, obj object.Object, expected float64) bool
 	return true
 }
 
-func testBooleanExpression(t *testing.T, obj object.Object, expected bool) bool {
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	result, ok := obj.(*object.Boolean)
 	if !ok {
 		t.Errorf("object is not Boolean, got=%T (%+v)", obj, obj)
@@ -55,6 +55,15 @@ func testBooleanExpression(t *testing.T, obj object.Object, expected bool) bool 
 
 	if result.Value != expected {
 		t.Errorf("object has wrong value, got=%t, want=%t", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("object is not Null, got=%T (%+v)", obj, obj)
 		return false
 	}
 
@@ -85,7 +94,7 @@ func TestEvalIntegerExpresson(t *testing.T) {
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
-		testIntegerExpression(t, evaluated, tt.expected)
+		testIntegerObject(t, evaluated, tt.expected)
 	}
 }
 
@@ -112,7 +121,7 @@ func TestEvalFloatExpresson(t *testing.T) {
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
-		testFloatExpression(t, evaluated, tt.expected)
+		testFloatObject(t, evaluated, tt.expected)
 	}
 }
 
@@ -144,7 +153,7 @@ func TestEvalBooleanExpresson(t *testing.T) {
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
-		testBooleanExpression(t, evaluated, tt.expected)
+		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
 
@@ -163,6 +172,32 @@ func TestEvalBangOperator(t *testing.T) {
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
-		testBooleanExpression(t, evaluated, tt.expected)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestIfElseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10.2 }", 10.2},
+		{"if (1 > 2) { 10 }", nil},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		if v, ok := tt.expected.(int); ok {
+			testIntegerObject(t, evaluated, int64(v))
+		} else if v, ok := tt.expected.(float64); ok {
+			testFloatObject(t, evaluated, float64(v))
+		} else {
+			testNullObject(t, evaluated)
+		}
 	}
 }
